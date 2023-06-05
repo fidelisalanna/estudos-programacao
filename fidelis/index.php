6a357a663be1ecfa9adc0c6096b808ebd1ecdb98
fidelixx <127751074+fidelisalanna@users.noreply.github.com>
2023-06-05 09:01:20 -0300

    Add files via upload

---
+++ fidelis/index.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	olá,
+	<?php
+		if(isset($_SESSION['nome'])==null){
+	?>
+		visitante<br>
+	<a href = "login.php">login</a>
+	<?php 
+	} else{ 
+		echo $_SESSION['nome']; ?>
+	<br><a href = "cadastro.php">cadastrar</a><br>
+	<a href = "logout.php">sair</a>
+	<?php } ?>	
+    
+</body>
+</html>
