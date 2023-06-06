d6611e2b5890e1f09652927a10a4ddfda45ce58f
fidelixx <127751074+fidelisalanna@users.noreply.github.com>
2023-06-06 11:02:38 -0300

    Add files via upload

--- fidelis/index.php
+++ fidelis/index.php
@@ -9,6 +9,7 @@ session_start();
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
 	<title></title>
 </head>
 <body>
